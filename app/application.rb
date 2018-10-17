9721bc2ed05af32c0005d7a79a78bccb4d30afe2
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-10-17 17:47:10 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -23,18 +23,15 @@ class Application
         search_term = req.params["item"]
         if !@@items.include?(search_term)
           @@cart << "#{search_term}"
-        resp.write "added #{search_term}"
-      else
-        resp.write "We don't have that item"
-      end
+        resp.write "added #{search_term}" 
+        else
+          resp.write "We don't have that item"
+        end
     else
       resp.write "Path Not Found"
     end
 
   resp.write "Your cart is empty" if @@cart.empty
-
-
-  end
 
     resp.finish
   end
