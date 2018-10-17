359817e2f2ae17a698f50d9cac7848950a008dcd
Zindzi Thompson <zindzi.thompson@gmail.com>
2018-10-17 17:20:41 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -15,7 +15,6 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
-    binding.pry
     else
       resp.write "Path Not Found"
     end
